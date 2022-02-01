f5f6de99287fe3b1271265ab27097842b8bdc0a1
Chinmai Sai Eshwar Reddy Kasi <chinmai.kasi55@gmail.com>
2022-02-01 15:21:06 -0500

    Part of #4 .Setup routes and created mock data for endpoints

--- backend/cmd/main/main.go
+++ backend/cmd/main/main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	routes "socialbytes.com/main/pkg/Routes"
+)
 
 func main() {
-
+	r := mux.NewRouter()
+	routes.RegisterEventRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
