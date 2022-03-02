9a7a6685f281deb516f75f4af3e8532aa2ddf47a
krish0307 <ktakkillapati@ufl.edu>
2022-03-02 18:47:11 -0500

    Revert "Krishna (#23)"
    This reverts commit 1d791ebe0882f27f2d7e69db73a949d162181ecb.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,21 +1,16 @@
-import { Box, Grid, Paper, Typography } from '@mui/material';
-import { useContext, useEffect } from 'react';
+import { styled, Typography } from '@mui/material';
+import { useContext } from 'react';
 import { Helmet } from 'react-helmet';
+import Tweet from '../utils/Tweet';
+
+import logo from '../logo.svg';
+
 import { AppContext } from '../contexts';
 import { APP_TITLE, PAGE_TITLE_HOME } from '../utils/constants';
-import { useGetPosts } from '../lib/api-hooks';
-import { FetchState } from '../types/post';
-import { styled } from '@mui/material/styles';
-import MediaCard from '../utils/MediaCard';
-import dataList from '../lib/data.json'
 
 export const Home = () => {
   const context = useContext(AppContext);
-  const [posts, fetchState, getPosts] = useGetPosts();
-  const StyledPaper = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-  }));
+
   return (
     <>
       <Helmet>
@@ -24,27 +19,16 @@ export const Home = () => {
         </title>
       </Helmet>
       <Typography variant="h4">{`Hello, ${context.user.name} `}</Typography>
-      <div className="jumbotron">
-        <h1>{"Hellow"}</h1>
-        <p>{"this.props.subText"}</p>
-        {/* <PrimaryButton buttonText='Learn more'/> */}
+      
+      <div style={{ display: "flex" }}>
+        <Tweet name="Gator night" type="Casual" />
+        <Tweet name="Football match" type="Sports event" />
+        <Tweet name="Grad ceremony" type="Academic"/>
+        <Tweet name="Basketball match" type="Sports event"/>
+
       </div>
-      <Box p={5}>
-        <Grid container spacing={5}>
-          {
-            dataList.map((post, i) => {
-              return (
-                <Grid key={i} item>
-                  <MediaCard {...post} />
-                </Grid>
-              );
-            })
-          }
-        </Grid>
-      </Box>
-
     </>
-
+    
   );
 };
 
