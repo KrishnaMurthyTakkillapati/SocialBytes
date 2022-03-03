1f949e3edcce64369099ea6a2e264f0191fdd582
Sohith Raja Buggaveeti <rajasohith@gmail.com>
2022-03-02 20:04:38 -0500

    Empty event details and Name unit test

--- backend/pkg/Models/event_test.go
+++ backend/pkg/Models/event_test.go
@@ -19,3 +19,15 @@ func TestCreateEventsTableEmptyDescription(t *testing.T) {
 	e.Details = "UF"
 	assert.Equal(t, e.CreateEventstable(), event)
 }
+func TestCreateEventsTableEmptyName(t *testing.T) {
+	var e Event
+	e.Description = "ABC"
+	e.Details = "UF"
+	assert.Equal(t, e.CreateEventstable(), event)
+}
+func TestCreateEventsTableEmptyDetails(t *testing.T) {
+	var e Event
+	e.Name = "ABC"
+	e.Description = "UF"
+	assert.Equal(t, e.CreateEventstable(), event)
+}
