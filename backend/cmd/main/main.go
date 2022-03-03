338bb90d81cdd9940d089a32b49296b05ec61cc5
Chinmai Sai Eshwar Reddy Kasi <chinmai.kasi55@gmail.com>
2022-03-03 15:25:53 -0500

    #21 swagger updates to base path

--- backend/cmd/main/main.go
+++ backend/cmd/main/main.go
@@ -5,16 +5,24 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger"
+	_ "socialbytes.com/main/cmd/main/docs"
 	routes "socialbytes.com/main/pkg/Routes"
 )
 
 // @title User API documentation
 // @version 1.0.0
-// @host localhost:9010w
+// @host localhost:9010
 // @BasePath /
 
 func main() {
 	r := mux.NewRouter()
+	r.PathPrefix("/swagger/").Handler(httpSwagger.Handler(
+		httpSwagger.URL("http://localhost:9010/swagger/doc.json"),
+		httpSwagger.DeepLinking(true),
+		httpSwagger.DocExpansion("none"),
+		httpSwagger.DomID("#swagger-ui"),
+	))
 	routes.RegisterEventRoutes(r)
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe("localhost:9010", r))
