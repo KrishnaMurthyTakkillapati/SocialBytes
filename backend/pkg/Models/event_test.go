4615306e4431f9af470a1a9f0ecd7325ddce2e48
Sohith Raja Buggaveeti <rajasohith@gmail.com>
2022-03-03 20:39:35 -0500

    added unit tests for GetEvents method

--- backend/pkg/Models/event_test.go
+++ backend/pkg/Models/event_test.go
@@ -8,6 +8,15 @@ import (
 
 var event *Event
 
+func TestCreateEventsTable(t *testing.T) {
+	var e Event
+	e.Name = "ABC"
+	e.Details = "UF"
+	e.Description = "UF"
+	res, _ := e.CreateEventstable()
+	assert.NotEqual(t, res, nil)
+}
+
 func TestCreateEventsTableEmpty(t *testing.T) {
 	_, err := event.CreateEventstable()
 
@@ -21,6 +30,7 @@ func TestCreateEventsTableEmptyDescription(t *testing.T) {
 	_, err := e.CreateEventstable()
 	assert.Equal(t, err.Error(), "Event details incorrect")
 }
+
 func TestCreateEventsTableEmptyName(t *testing.T) {
 	var e Event
 	e.Description = "ABC"
@@ -28,6 +38,7 @@ func TestCreateEventsTableEmptyName(t *testing.T) {
 	_, err := e.CreateEventstable()
 	assert.Equal(t, err.Error(), "Event details incorrect")
 }
+
 func TestCreateEventsTableEmptyDetails(t *testing.T) {
 	var e Event
 	e.Name = "ABC"
@@ -36,14 +47,6 @@ func TestCreateEventsTableEmptyDetails(t *testing.T) {
 	assert.Equal(t, err.Error(), "Event details incorrect")
 }
 
-func TestCreateEventsTable(t *testing.T) {
-	var e Event
-	e.Name = "ABC"
-	e.Details = "UF"
-	e.Description = "UF"
-	res, _ := e.CreateEventstable()
-	assert.NotEqual(t, res, nil)
-}
 func TestGetEventsTable(t *testing.T) {
 	res := GetAllEvents()
 	assert.NotEqual(t, len(res), 0)
