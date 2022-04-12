89be129cdf6f3c9fd9bb37456bcae40d38f5d6db
krish0307 <ktakkillapati@ufl.edu>
2022-04-11 23:27:54 -0400

    Update to login functionality

--- frontend/src/Register.tsx
+++ frontend/src/Register.tsx
@@ -1,8 +1,8 @@
 import React, { useReducer, useEffect, SyntheticEvent } from 'react';
 import { createStyles, makeStyles, } from '@mui/styles';
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-import {Theme } from '@mui/system';
+import { Theme } from '@mui/system';
 
 import TextField from '@mui/material/TextField';
 import Card from '@mui/material/Card';
@@ -11,10 +11,6 @@ import CardActions from '@mui/material/CardActions';
 import CardHeader from '@mui/material/CardHeader';
 import Button from '@mui/material/Button';
 import axios from "axios";
-
-
-
-
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -43,18 +39,22 @@ const useStyles = makeStyles((theme: Theme) =>
 
 type State = {
   username: string
-  password:  string
+  password: string
   isButtonDisabled: boolean
   helperText: string
   isError: boolean
+  firstName: string
+  lastName: string
 };
 
-const initialState:State = {
+const initialState: State = {
   username: '',
   password: '',
   isButtonDisabled: true,
   helperText: '',
-  isError: false
+  isError: false,
+  firstName: '',
+  lastName: ''
 };
 
 type Action = { type: 'setUsername', payload: string }
@@ -62,41 +62,53 @@ type Action = { type: 'setUsername', payload: string }
   | { type: 'setIsButtonDisabled', payload: boolean }
   | { type: 'loginSuccess', payload: string }
   | { type: 'loginFailed', payload: string }
-  | { type: 'setIsError', payload: boolean };
+  | { type: 'setIsError', payload: boolean }
+  | { type: 'setFirstName', payload: string }
+  | { type: 'setLastName', payload: string };
 
 const reducer = (state: State, action: Action): State => {
   switch (action.type) {
-    case 'setUsername': 
+    case 'setUsername':
       return {
         ...state,
         username: action.payload
       };
-    case 'setPassword': 
+    case 'setPassword':
       return {
         ...state,
         password: action.payload
       };
-    case 'setIsButtonDisabled': 
+    case 'setIsButtonDisabled':
       return {
         ...state,
         isButtonDisabled: action.payload
       };
-    case 'loginSuccess': 
+    case 'loginSuccess':
       return {
         ...state,
         helperText: action.payload,
         isError: false
       };
-    case 'loginFailed': 
+    case 'loginFailed':
       return {
         ...state,
         helperText: action.payload,
         isError: true
       };
-    case 'setIsError': 
+    case 'setIsError':
       return {
         ...state,
         isError: action.payload
+      };
+    case 'setFirstName':
+      return {
+        ...state,
+        firstName: action.payload
+      };
+    case 'setLastName':
+      return {
+        ...state,
+        lastName: action.payload
       };
   }
 }
@@ -107,10 +119,10 @@ const Register = () => {
 
   useEffect(() => {
     if (state.username.trim() && state.password.trim()) {
-     dispatch({
-       type: 'setIsButtonDisabled',
-       payload: false
-     });
+      dispatch({
+        type: 'setIsButtonDisabled',
+        payload: false
+      });
     } else {
       dispatch({
         type: 'setIsButtonDisabled',
@@ -119,35 +131,48 @@ const Register = () => {
     }
   }, [state.username, state.password]);
 
-  
+
   const history = useHistory();
-  const handleRegister = async(event: SyntheticEvent) => {
-        event.preventDefault()
+  const handleRegister = async (event: SyntheticEvent) => {
+    event.preventDefault()
+console.log(    JSON.stringify({ 'ID':2,'FirstName': state.firstName, 'LastName': state.lastName, 'Email': state.username, 'Password': state.password }))
+    axios.post(`http://localhost:9010/api/createUser`, 
+    JSON.stringify({ 'FirstName': state.firstName, 'LastName': state.lastName, 'Email': state.username, 'Password': state.password }))
+    .then(response => {
+      console.log(response.headers['set-cookie'])
+      if (response.status != 200) {
+        // const error = (data && data.message) || response.statusText;
+        return Promise.reject(response.statusText);
+      }
+      return (response.status);
+    });
 
-        alert('Rerouting ');
-    
-        history.push("/Login");
 
-        //interact with the backend
-            
-            // return JSON.stringify({"status" : "OK"})
-            
+
+    // alert('Rerouting ');
+
+    // history.push("/Login");
+
+    //interact with the backend
+
+    // return JSON.stringify({"status" : "OK"})
+
   }
-  
 
 
-    // if (state.username === 'abc@email.com' && state.password === 'password') {
-    //   dispatch({
-    //     type: 'loginSuccess',
-    //     payload: 'Login Successfully'
-    //   });
-    // } else {
-    //   dispatch({
-    //     type: 'loginFailed',
-    //     payload: 'Incorrect username or password'
-    //   });
-    // }
-  
+
+  // if (state.username === 'abc@email.com' && state.password === 'password') {
+  //   dispatch({
+  //     type: 'loginSuccess',
+  //     payload: 'Login Successfully'
+  //   });
+  // } else {
+  //   dispatch({
+  //     type: 'loginFailed',
+  //     payload: 'Incorrect username or password'
+  //   });
+  // }
+
 
   const handleKeyPress = (event: React.KeyboardEvent) => {
     if (event.keyCode === 13 || event.which === 13) {
@@ -170,13 +195,29 @@ const Register = () => {
         payload: event.target.value
       });
     }
+
+  const handleFirstNameChange: React.ChangeEventHandler<HTMLInputElement> =
+    (event) => {
+      dispatch({
+        type: 'setFirstName',
+        payload: event.target.value
+      });
+    }
+
+  const handleLastNameChange: React.ChangeEventHandler<HTMLInputElement> =
+    (event) => {
+      dispatch({
+        type: 'setLastName',
+        payload: event.target.value
+      });
+    }
   return (<>
     <form className={classes.container} noValidate autoComplete="off">
       <Card className={classes.card}>
         <CardHeader className={classes.header} title="Register" />
         <CardContent>
           <div>
-          <TextField
+            <TextField
               error={state.isError}
               fullWidth
               id="firstName"
@@ -184,19 +225,19 @@ const Register = () => {
               label="First Name"
               placeholder="Type your FirstName"
               margin="normal"
-              onChange={handleUsernameChange}
+              onChange={handleFirstNameChange}
               onKeyPress={handleKeyPress}
             /><TextField
-            error={state.isError}
-            fullWidth
-            id="lastName"
-            type="lastName"
-            label="Last Name"
-            placeholder="Type your Last Name"
-            margin="normal"
-            onChange={handleUsernameChange}
-            onKeyPress={handleKeyPress}
-          />
+              error={state.isError}
+              fullWidth
+              id="lastName"
+              type="lastName"
+              label="Last Name"
+              placeholder="Type your Last Name"
+              margin="normal"
+              onChange={handleLastNameChange}
+              onKeyPress={handleKeyPress}
+            />
             <TextField
               error={state.isError}
               fullWidth
@@ -247,7 +288,7 @@ const Register = () => {
         </CardActions>
       </Card>
     </form>
-    </>
+  </>
   );
 }
 
