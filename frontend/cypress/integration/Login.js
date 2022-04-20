3fac2049d04c07ab693a73e4b5921723cf48c313
Venkata Vynatheya Jangal <venkatvynatheya@Venkatas-MacBook-Pro.local>
2022-04-20 14:48:37 -0400

    Updated cypress test cases for Login and Register

---
+++ frontend/cypress/integration/Login.js
@@ -0,0 +1,54 @@
+describe('Social Bytes', () => {
+    it('Visits website', function() {
+    
+        cy.visit('http://localhost:3000')
+        
+        
+    
+        
+      })
+      it('Checks if SignUp button is clicked', function() {
+
+
+
+    
+    
+        cy.visit('http://localhost:3000/Login')
+      })
+    //   it('Checks if SignUp button is clicked', function() {
+
+
+
+    
+    
+    //     cy.visit('http://localhost:3000/Register')
+    //   })
+      it('Checks if Username is filled', function() {
+
+
+
+        cy.get('label').contains('Username').click({force: true}).type('eshwar@gmail.com');
+        
+        
+    
+    })
+    it('Checks if password is filled', function() {
+
+
+
+        cy.get('label').contains('Password').click({force: true}).type('Website123');
+        
+        
+    
+    })
+     it('Checks if Register button is clicked', function() {
+
+
+
+            cy.get('button').contains('Login').click();
+            
+            
+        
+        })
+
+    })
---
+++ frontend/cypress/integration/Register.js
@@ -0,0 +1,99 @@
+describe('Social Bytes', () => {
+    it('Visits website', function() {
+    
+        cy.visit('http://localhost:3000')
+        
+        
+    
+        
+      })
+      it('Checks if SignUp button is clicked', function() {
+
+
+
+    
+    
+        cy.visit('http://localhost:3000/Register')
+      })
+
+      it('Checks if firstName is filled', function() {
+
+
+
+            cy.get('label').contains('First Name').click({force: true}).type('Krishna');
+            
+            
+        
+        })
+        it('Checks if lastname is filled', function() {
+
+
+
+            cy.get('label').contains('Last Name').click({force: true}).type('Vamsi');
+            
+            
+        
+        })
+        it('Checks if username is filled', function() {
+
+
+
+            cy.get('label').contains('Create Username').click({force: true}).type('venky@gmail.com');
+            
+            
+        
+        })
+        it('Checks if password is filled', function() {
+
+
+
+            cy.get('label').contains('Create Password').click({force: true}).type('123456789');
+            
+            
+        
+        })
+        // it('Checks if password is retyped', function() {
+
+
+
+        //     cy.get('[id="password-label"]').click();
+            
+            
+        
+        // })
+
+        it('Checks if Register button is clicked', function() {
+
+
+
+            cy.get('button').contains('Register').click();
+            
+            
+        
+        })
+        
+       
+
+
+//       it('Checks if My account settings button is clicked', function() {
+
+
+
+    
+    
+//         cy.get('a[href*="/account/settings"]').click();
+
+// })
+// it('Checks if firstName is filled', function() {
+
+
+
+//     cy.get('label').contains('firstName').click({force: true}).type('Krishna');
+    
+    
+
+// })
+
+
+
+})
